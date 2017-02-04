584fc213fdcb4ef9716ab2fb26c55a1e56afbe5c
Kieran Lamb <Kieran Lamb>
2017-02-04 14:38:14 +0000

    Up to chapter 6.3

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    view = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category,self).save(*args, **kwargs)
+        
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.name
+    
+class Page(models.Model):
+
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.title
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,5 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from rango.models import Category
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+    category_list=Category.objects.order_by('-likes')[:5]
+    
+    context_dict = {'categories':category_list}
+    
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': 'This page is all about Rango!'}
+    return render(request, 'rango/about.html', context_dict)
