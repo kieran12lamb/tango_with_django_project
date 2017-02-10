31478df2bb4113f34eea4bad452f00d0a53f1c8c
Kieran Lamb <Kieran Lamb>
2017-02-10 16:56:48 +0000

    final push

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -11,25 +11,28 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
 
-def index(request):
 
+                            
+def index(request):
     request.session.set_test_cookie()
     category_list=Category.objects.order_by('-likes')[:5]
     page_list=Page.objects.order_by('-views')[:5]
-    context_dict = {'categories':category_list,
-                    'pages':page_list}
+    context_dict = {'categories':category_list,'pages':page_list}
 
     visitor_cookie_handler(request)
     context_dict['visits']=request.session['visits']
-
-    response = render(request,'rango/index.html',context=context_dict)
+    
+    response = render(request,'rango/index.html',context = context_dict)
+    
     return response
 
 def about(request):
     if request.session.test_cookie_worked():
         print("test cookie worked!")
         request.session.delete_test_cookie()
-    context_dict = {'boldmessage': 'This page is all about Rango!'}
+    visitor_cookie_handler(request)
+    context_dict = {'boldmessage': 'This page is all about Rango!',
+                    'Views':request.session['visits']}
     return render(request, 'rango/about.html', context_dict)
 
 def show_category(request,category_name_slug):
@@ -156,7 +159,6 @@ def get_server_side_cookie(request,cookie,default_val=None):
 
 def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request,'visits','1'))
-
     last_visit_cookie = get_server_side_cookie(request,
                                                'last_visit',
                                                str(datetime.now()))
@@ -171,5 +173,5 @@ def visitor_cookie_handler(request):
         request.session['last_visit']= last_visit_cookie
 
     request.session['visits']=visits
-                            
+    
     
